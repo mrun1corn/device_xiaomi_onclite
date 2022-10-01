d3127b3988bb053fd160781eccc8cf8ff8c665de
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-10-01 22:02:20 +0530

    onclite: Build android.hardware.bluetooth.audio-impl

--- device.mk
+++ device.mk
@@ -114,6 +114,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     audio.bluetooth.default \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
