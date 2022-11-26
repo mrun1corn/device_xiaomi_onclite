432b8e150dc84882706ef0144d9447e927905b3c
mrun1corn <robinisalegend01@gmail.com>
2022-11-26 08:52:46 +0600

    onclite: back to QTI power hal
    This reverts commit 1f0a5c54991cfe6519bb71f4001fe9439a76a08a.

--- device.mk
+++ device.mk
@@ -341,7 +341,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-service \
+    android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor \
     android.hardware.power@1.2.vendor
 
