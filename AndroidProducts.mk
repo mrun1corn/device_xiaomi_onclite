91150e49e6d1e5b724b062b3f39fe1b39188c762
mrun1corn <robinisalegend01@gmail.com>
2023-02-23 09:31:30 +0100

    onclite: bringup derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_onclite-user \
-    lineage_onclite-userdebug \
-    lineage_onclite-eng
+    $(LOCAL_DIR)/derp_onclite.mk
